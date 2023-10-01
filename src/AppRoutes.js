c38419200d8a449ccc795416990601edf9f76545
sayedanbia1 <sayedanbia5555@gamil.com>
2023-10-01 20:12:28 +0330

    thired-commit

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -3,6 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home/HomePage';
 import FoodPage from './pages/Food/FoodPage';
 import CartPage from './pages/Cart/CartPage';
+import LoginPage from './pages/Login/LoginPage';
+import RegisterPage from './pages/Register/RegisterPage';
+import AuthRoute from './components/AuthRoute/AuthRoute';
+import CheckoutPage from './pages/Checkout/CheckoutPage';
 
 export default function AppRoutes() {
   return (
@@ -12,6 +16,16 @@ export default function AppRoutes() {
       <Route path="/tag/:tag" element={<HomePage />} />
       <Route path="/food/:id" element={<FoodPage />} />
       <Route path="/cart" element={<CartPage />} />
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path="/register" element={<RegisterPage />} />
+      <Route
+        path="/checkout"
+        element={
+          <AuthRoute>
+            <CheckoutPage />
+          </AuthRoute>
+        }
+      />
     </Routes>
   );
 }
