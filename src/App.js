aa6a22e29401220e81a260a1230d3d8b776a48c9
sayedanbia1 <sayedanbia5555@gamil.com>
2023-10-01 15:49:51 +0330

    thired-commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
+import Header from './components/Header/Header';
+import AppRoutes from './AppRoutes';
 
 function App() {
   return (
-    <div className="App">
+   
         <>
-        hello
+        <Header/>
+        <AppRoutes />
         </>
-    </div>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import classes from './header.module.css';
+import { Link } from 'react-router-dom';
+import { useCart } from '../../hooks/useCart';
+
+export default function Header() {
+  const user={
+    name:'seyyed Anbia'
+  }
+  const { cart } = useCart();
+  const logout=()=>{}
+  return (
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <Link to='/' className={classes.logo}>
+        Home
+        </Link>
+        <nav>
+          <ul>
+            {user ? (
+              <li className={classes.menu_container}>
+                <Link to="/profile">{user.name}</Link>
+                <div className={classes.menu}>
+                  <Link to="/profile">Profile</Link>
+                  <Link to="/orders">Orders</Link>
+                  <a href='/login' onClick={logout}>logout</a>
+                </div>
+              </li>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+
+            <li>
+              <Link to="/cart">
+                Cart
+                {cart.totalCount > 0 && (
+                  <span className={classes.cart_count}>{cart.totalCount}</span>
+                )}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
